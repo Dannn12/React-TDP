9d76e338bc11ddca47c39b6de878b3fbb8414b6d
Dannn12 <daniel.murden@icloud.com>
2022-10-19 11:29:10 +0100

    Temporary state

--- continuedselflearn/src/App.jsx
+++ continuedselflearn/src/App.jsx
@@ -1,26 +1,43 @@
 import './App.css';
+
+
+import {
+  BrowserRouter as Router, Link, Route, Routes,
+} from 'react-router-dom';
 import MyComponent from './MyComponent';
 import ComponentWithProps from './ComponentWithProps';
 import Pet from './Pet.jsx'
+import Nav from '.nav.jsx'
 
 const App = () => {
-return (
+  return (
     <div className="App">
-      <MyComponent />
-      <ComponentWithProps 
-      headerProp= "Hello my name is dan" 
-      contentProp= "Some really useless content" 
-      numbercontentProp= "1000 Thank yous George for your patience!"
-      nonExcistant= "       lots of space"  /> 
+      <Router>
+        <header>
+          <h1>Welcome to the world of tomorrow!!!</h1>
+        </header>
+        <nav>
+          <Link to="/ComponentWithProps">Component</Link>
+          <Link to="/Pet">Pet</Link>
+         </nav>
+        <Routes>
+        <Nav />
+        </Routes>
+        <MyComponent />
+        <ComponentWithProps
+          headerProp="Hello my name is dan"
+          contentProp="Some really useless content"
+          numbercontentProp="1000 Thank yous George for your patience!"
+          nonExcistant="       lots of space" />
 
-      <Pet name= "Mittens" animal="cat" height="70cm" food="mice" />
-      <Pet name= "Mittens" animal="cat" height="70cm" food="mice" />
-      <Pet name= "Mittens" animal="cat" height="70cm" food="mice" />
+        <Pet name="Mittens" animal="cat" height="70cm" food="mice" />
+        <Pet name="Mittens" animal="cat" height="70cm" food="mice" />
+        <Pet name="Mittens" animal="cat" height="70cm" food="mice" />
 
-      <Pet name= "Scooby" animal="dog" height="200cm" food="ScoobySnacks"/>
-      <Pet name= "Ghost" anmimal="direwolf" height="240cm" food="Lannisters?"/>
-      <Pet name= "Baa" animal="Sheep" height="220cm" food="Grass"/>
-
+        <Pet name="Scooby" animal="dog" height="200cm" food="ScoobySnacks" />
+        <Pet name="Ghost" anmimal="direwolf" height="240cm" food="Lannisters?" />
+        <Pet name="Baa" animal="Sheep" height="220cm" food="Grass" />
+      </Router>
     </div>
   );
 }
---
+++ continuedselflearn/src/nav.jsx
@@ -0,0 +1,10 @@
+import {Link} from 'react-router-dom';
+
+const Nav = () => {
+    <>
+    <Link to="/ComponentWithProps">Component</Link>
+    <Link to="/Pet">Pet</Link>
+    </>
+}
+
+export default Nav;
